4f3ff25f1fd2eee91b9f2965d94417ae464d11b3
NewWorldOrder <nwokedi@gmail.com>
2015-07-05 21:03:08 -0700

    handle emacs additional backup files issues, and include a cleaner way to include stopwords

--- analyze.py
+++ analyze.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from scipy.misc import imread  
-from wordcloud import WordCloud
+from wordcloud import WordCloud, STOPWORDS
 
 if __name__ == '__main__':
     filename = 'data/tidy_redlist.csv'
@@ -9,11 +9,10 @@ if __name__ == '__main__':
         lines = f.readlines()
     responses = [line.split('|')[1] for line in lines]
     text = ' '.join(responses)
-    text = text.lower()
-    text = text.replace('security', '')  # because 'security' is uninformative
-    # image pulled from: http://www.stencilry.org/stencils/zombies/zombie-1.gif
+    STOPWORDS.add('security')  # 'security' is uninformative 
+    # image source: http://www.stencilry.org/stencils/zombies/zombie-1.gif
     zombie_mask = imread('data/zombie.jpg')
-    wc = WordCloud(mask = zombie_mask).generate(text)
+    wc = WordCloud(mask = zombie_mask, stopwords=STOPWORDS).generate(text)
 
     # show the word cloud 
     plt.imshow(wc)
